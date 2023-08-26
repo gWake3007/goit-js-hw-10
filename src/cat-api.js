f226f2f7c23c581cf34a7d2fdd951d8257604f7f
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2023-08-26 19:14:07 +0200

    123

---
+++ src/cat-api.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import Notiflix from "notiflix";
+axios.defaults.baseURL = 'https://api.thecatapi.com/v1/'; 
+axios.defaults.headers.common["x-api-key"] = "live_EXhJAjPTJ745YLEpTpYemXM89BIqElJKKE5nFEeskbsHZWVqnkY1gNCyMTCPH0fK";
+
+
+export {fetchBreeds};
+export {fetchCatByBreed};
+
+function fetchBreeds() {
+
+  return axios.get('breeds')
+    .then(response => {
+
+      return response.data;
+    })
+}
+
+
+function fetchCatByBreed(breedId) {
+  
+  return axios.get(`images/search?breed_ids=${breedId}`)
+  .then(response => {
+
+    return response.data;
+  })
+  .catch(error => {
+
+    Notiflix.Notify.failure("Oops! Something went wrong! Try reloading the page!");
+  });
+}
---
+++ src/index.js
@@ -0,0 +1,77 @@
+import { fetchBreeds, fetchCatByBreed} from "./cat-api";
+import Notiflix from "notiflix";
+
+
+// Dom
+    const breedSelect = document.querySelector(".breed-select");
+    const catInfo = document.querySelector(".cat-info");
+    const loader = document.querySelector(".loader");
+    // let breeds = {};
+ 
+
+// Event listener 
+
+breedSelect.addEventListener("change", fetchCatInfo);
+
+
+// Fetch the list of breed
+
+fetchBreeds()
+.then((data) => renderMarkup(data))
+.catch(() => {
+
+    Notiflix.Notify.failure("Oops! Something went wrong! Try reloading the page!");
+});
+
+
+//Render the list 
+
+function renderMarkup(data) {
+  
+    // const breeds = data;
+    breedSelect.classList.remove("hidden");
+    loader.classList.add("hidden");
+    
+    breedSelect.innerHTML = data.reduce((html,el) => {
+        return html+`<option value="${el.id}">${el.name}</option>`
+      },"");
+    
+}
+
+//Fetch elements on the list 
+
+function fetchCatInfo() {
+
+    catInfo.classList.add("hidden");
+     loader.classList.remove("hidden");
+
+    const selectedValue = breedSelect.value;
+
+    fetchCatByBreed(selectedValue)
+    .then((data) => renderCatInfo(data))
+    .catch(() => {
+
+        Notiflix.Notify.failure("Oops! Something went wrong! Try reloading the page!");
+    });
+}
+
+//Rendering elements on the lists
+
+function renderCatInfo(data) {
+    
+    catInfo.classList.remove("hidden");
+    loader.classList.add("hidden");
+    
+
+    const name = data[0].breeds[0].name;
+    const description = data[0].breeds[0].description;
+    const temperament = data[0].breeds[0].temperament;
+   
+    const markup = `<img src="${data[0].url}" alt="${name}" width="350">
+    <div>
+    <h3>${name}</h3>
+        <p>${description}</p>
+        <p><span class="temper">Temperament: </span>${temperament}</p>
+        </div>`
+    catInfo.innerHTML = markup;
+}
