b0314df7f501ccf5335e9855552e560b4b971909
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2023-08-26 22:41:01 +0200

    123

--- src/cat-api.js
+++ src/cat-api.js
@@ -3,7 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://api.thecatapi.com/v1/'; 
 axios.defaults.headers.common["x-api-key"] = "live_EXhJAjPTJ745YLEpTpYemXM89BIqElJKKE5nFEeskbsHZWVqnkY1gNCyMTCPH0fK";
 
-
 export {fetchBreeds};
 export {fetchCatByBreed};
 
