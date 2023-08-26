df5f2962ae24511ccab3e5657d880c5c9c11120e
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2023-08-27 00:41:39 +0200

    123

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,5 +1,5 @@
-import Notiflix from "notiflix";
 import axios from "axios";
+import Notiflix from "notiflix";
 axios.defaults.baseURL = 'https://api.thecatapi.com/v1/'; 
 axios.defaults.headers.common["x-api-key"] = "live_EXhJAjPTJ745YLEpTpYemXM89BIqElJKKE5nFEeskbsHZWVqnkY1gNCyMTCPH0fK";
 
